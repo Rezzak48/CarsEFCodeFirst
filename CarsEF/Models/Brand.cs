ff94f46ce4a5965a4fb28f64fba52c47144570d8
MOHAMED ERRAZAK ALLAH <62334356+Rezzak48@users.noreply.github.com>
2022-11-01 17:21:26 +0100

    Add Model classes

---
+++ CarsEF/Models/Brand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsEF.Models
+{
+    [Table("brands")] // That's specify the class is maped only to the brands database
+    public class Brand
+    {
+        [Key] // specify as a key
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Generate an id
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        [Required] // means can't be null
+        public string Name { get; set; }
+
+        //In a brand we can have many cars, so we use a collection
+        [NotMapped] // doesn't have to look in database !!!Navigation propery!!!
+        // Whenever we use a navigation property, the method should be virtual
+        public virtual ICollection<Car> Cars { get; set; } // ICollection at the end there is a foreign key relationship
+
+        public Brand()
+        {
+            Cars = new HashSet<Car>();
+        }
+    
+    
+    }
+}
---
+++ CarsEF/Models/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsEF.Models
+{
+    [Table("cars")]
+    public class Car
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("car_id", TypeName = "int")] // specify the column
+        public int Id { get; set; }
+        [MaxLength(100)]
+        [Required]
+        public string Model { get; set; }
+        public int? BestPrice { get; set; }
+        [ForeignKey(nameof(Brand))] // refer to the brand
+        public int BrandId { get; set; }
+
+        [NotMapped]
+        public virtual Brand Brand { get; set; }
+    }
+}
