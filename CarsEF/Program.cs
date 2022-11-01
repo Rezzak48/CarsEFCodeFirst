044fa19651cacfc18fcbab4ceaf708e09c4ee53c
MOHAMED ERRAZAK ALLAH <62334356+Rezzak48@users.noreply.github.com>
2022-11-01 13:50:06 +0100

    Added new solution and project

---
+++ CarsEF/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarsEF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
