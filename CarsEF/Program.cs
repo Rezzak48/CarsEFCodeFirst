24790b9136d3c5e93f029c719e8ea02ac94b64f4
MOHAMED ERRAZAK ALLAH <62334356+Rezzak48@users.noreply.github.com>
2022-11-01 17:22:05 +0100

    change program cs

--- CarsEF/Program.cs
+++ CarsEF/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using CarsEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CarsEF
 {
+    static class Operation
+    {
+        //Dislay helper
+        public static void ToConsole<T>(this IEnumerable<T> input, string str)
+        {
+            Console.WriteLine($" ****** BEGIN {str} ****** ");
+            foreach (var item in input) Console.WriteLine(item);
+            Console.WriteLine($" ****** END {str} ****** ");
+            Console.ReadLine();
+        }
+    }
     internal class Program
     {
+        // disconnect to the database and remove the connection if you are using code first approach
+        // Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory (relative path)|
+        // ;Integrated Security=True;
+        // add also : MultipleActiveResultSets = true
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Added EF : core, proxis, tool, sqlserver
+            // Add The DB // change Build to content & copy always
+
+            CarDbContext ctx = new CarDbContext();
+            ctx.Brands.Select(x => x.Name).ToConsole("Brands");
+            ctx.Cars.Select(x => $"{ x.Model} from {x.Brand.Name}").ToConsole("Cars");
+
         }
     }
 }
